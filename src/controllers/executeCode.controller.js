1ac1cf2f5d7d9001d0a17e5fa74418c635e17a3c
Shubham Singh <shubham@techtoabroad.com>
2025-05-25 19:18:02 +0530

    Code execution logic completed

--- src/controllers/executeCode.controller.js
+++ src/controllers/executeCode.controller.js
@@ -1,5 +1,49 @@
-import { submitBatch } from "../libs/judge0.libs";
+import { pollBatchResults, submitBatch } from "../libs/judge0.libs.js";
 
-export const executeCode = async (req,res) => {
+export const executeCode = async (req, res) => {
+  try {
+    const { source_code, language_id, stdin, expected_outputs, problemId } =
+      req.body;
+    const userId = req.user.id;
 
-}
+    //validate test cases
+
+    if (
+      !Array.isArray(stdin) ||
+      stdin.length === 0 ||
+      !Array.isArray(expected_outputs) ||
+      expected_outputs.length !== stdin.length
+    ) {
+      return res.status(400).json({
+        error: "Invalid or missing test cases",
+      });
+    }
+
+    //Prepare each test case for Judge0 batch submission
+
+    const submissions = stdin.map((input) => ({
+      source_code,
+      language_id,
+      stdin: input,
+    }));
+
+    // Send batch of submissons to Judge0
+
+    const submitResponse = await submitBatch(submissions);
+
+    const tokens = submitResponse.map((res) => res.token);
+
+    // Poll Judge0 for results of all submitted test cases
+    const results = await pollBatchResults(tokens);
+    console.log("results---->>>", results);
+
+    res.status(200).json({
+      message: "Code executed successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Error in code exucution",
+      error,
+    });
+  }
+};
--- src/routes/executeCode.routes.js
+++ src/routes/executeCode.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { authMiddleware } from "../middleware/auth.middleware";
-import { executeCode } from "../controllers/executeCode.controller";
+import { authMiddleware } from "../middleware/auth.middleware.js";
+import { executeCode } from "../controllers/executeCode.controller.js";
 
 const executionRoute = express.Router();
 
