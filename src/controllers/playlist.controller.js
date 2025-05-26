8ecfefea2a03e74889bcddfcd8d7e54ce2c4bd02
Shubham Singh <shubham@techtoabroad.com>
2025-05-26 12:41:04 +0530

    getPlaylistDetails controllers code added

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -34,34 +34,85 @@ export const createPlaylist = async (req, res) => {
 };
 
 export const getAllListDetails = async (req, res) => {
-    try {
-        const userId=req.user.id
+  try {
+    const userId = req.user.id;
 
-        const playlists = await db.playlist.findMany({
-            where:{
-                userId
-            },
-            include:{
-                problems:{
-                    include:{
-                        problem:true,
-                    }
-                }
-            }
-        })
-        res.status(200).json({
-            success:true,
-            message: "Playlists fetched successfully",
-            playlists
-        })
-    } catch (error) {
-        res.status(500).json({
-            error:"Failed to fetch playlist"
-        })
-    }
+    const playlists = await db.playlist.findMany({
+      where: {
+        userId,
+      },
+      include: {
+        problems: {
+          include: {
+            problem: true,
+          },
+        },
+      },
+    });
+    res.status(200).json({
+      success: true,
+      message: "Playlists fetched successfully",
+      playlists,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Failed to fetch playlist",
+    });
+  }
 };
 
-export const getPlaylistDetails = async (req, res) => {};
+export const getPlaylistDetails = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { playlistId } = req.params;
+
+    if (!playlistId) {
+      return (
+        res.status(400),
+        json({
+          success: false,
+          message: "PlaylistId is missing",
+        })
+      );
+    }
+
+    const playlist = await db.playlist.findUnique({
+      where: {
+        id: playlistId,
+        userId,
+      },
+      include: {
+        problems: {
+          include: {
+            problem: true,
+          },
+        },
+      },
+    });
+
+    if (!playlist) {
+      return (
+        res.status(400),
+        json({
+          success: false,
+          message: "Playlist not found",
+        })
+      );
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Playlist fetched successfully",
+      playlist,
+    });
+  } catch (error) {
+    res.status(500),
+      json({
+        success: false,
+        message: "Failed to fetch playlist",
+      });
+  }
+};
 export const addProblemToPlaylist = async (req, res) => {};
 export const deletePlaylist = async (req, res) => {};
 export const removeProblemFromPlaylist = async (req, res) => {};
