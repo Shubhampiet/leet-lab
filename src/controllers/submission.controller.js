cdde086e9d8b9d46bcff0c6078c459bfb07c9f1f
Shubham Singh <shubham@techtoabroad.com>
2025-05-26 09:44:09 +0530

    submission controller defined

---
+++ src/controllers/submission.controller.js
@@ -0,0 +1,5 @@
+export const getAllSubmissions = async (req, res) => {};
+
+export const getSubmissionForProblem = async (req, res) => {};
+
+export const getAllTheSubmissionsForProblem = async (req, res) => {};
