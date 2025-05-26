c0ba27ec16ffb7203d8c0efdec833eb95f4a08b9
Shubham Singh <shubham@techtoabroad.com>
2025-05-26 11:49:04 +0530

    playlist routes added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import cookieparser from "cookie-parser";
 import problemRoutes from "./routes/problem.routes.js";
 import executionRoute from "./routes/executeCode.routes.js";
 import submissionRoutes from "./routes/submission.routes.js";
+import playlistRoutes from "./routes/playlist.routes.js";
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/problem", problemRoutes)
 app.use("/api/v1/execute-code", executionRoute)
 app.use("/api/v1/submission", submissionRoutes)
+app.use("/api/v1/playlist", playlistRoutes)
 
 app.listen(port, () => {
   console.log("App is listening on port", port);
