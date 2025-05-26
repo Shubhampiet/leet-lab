4d6dbb192cd4763c853813d262d7733e70bcef6e
Shubham Singh <shubham@techtoabroad.com>
2025-05-26 12:17:51 +0530

    create-playlist controller code added

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,6 +1,40 @@
 import { db } from "../libs/db.js";
 
-export const createPlaylist = async (req, res) => {};
+export const createPlaylist = async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    const userId = req.user.id;
+    console.log("userId>>>>>>", userId)
+
+    if (!name) {
+      return (
+        res.status(400),
+        json({
+          success: false,
+          message: "Playlist name is missing",
+        })
+      );
+    }
+
+    const playlist = await db.playlist.create({
+      data: {
+        name,
+        description,
+        userId,
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Playlist created successsfully",
+      playlist,
+    });
+  } catch (error) {
+    console.log("createPlaylist error", error);
+    res.status(500).json({ error: "Failed to create playlist" });
+  }
+};
+
 export const getAllListDetails = async (req, res) => {};
 export const getPlaylistDetails = async (req, res) => {};
 export const addProblemToPlaylist = async (req, res) => {};
